45544ff248d5a38106e0d591eb51f646c6f8e4c6
Matthew Fennell <mf2017@ic.ac.uk>
2020-12-21 01:35:42 +0000

    Connect backend to the database - no longer return hardcoded session data

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express")
 const cors = require("cors")
 const port = process.env.PORT || 3000
-
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://matthew:Yq3gdiOOCgVRY411@smr-dashboard.wbqeq.mongodb.net/smr-dashboard?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express()
-
 app.use(cors())
 
-app.get("/cars", (req, res) => {
-	res.json([{ name: "#92 SpeedyMite Racing" }])
+app.get("/sessions", (req, res) => {
+	client.connect(err => {
+		const collection = client.db("dashboard").collection("sessions")
+		collection.find({name: "VEC 2020 Race 4"}, (err, cursor) => {
+			cursor.toArray((err, cars) => {
+				res.send(cars)
+				client.close()
+			})
+		})
+	})
 })
 
 app.listen(port, () => {
