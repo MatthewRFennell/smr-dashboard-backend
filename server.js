a09bafdcab398df2e4da3580ce330f66aa46149d
Matthew Fennell <mf2017@ic.ac.uk>
2020-12-21 00:24:59 +0000

    Add CORS

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const cors = require("cors")
 const port = process.env.PORT || 3000
 
 const app = express()
+
+app.use(cors())
 
 app.get("/cars", (req, res) => {
 	res.json({ name: "#92 SpeedyMite Racing" })
