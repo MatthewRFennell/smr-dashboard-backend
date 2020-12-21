58f39dd493b746eb7f34e04e1a3a71cb7bb80989
Matthew Fennell <mf2017@ic.ac.uk>
2020-12-21 03:26:58 +0000

    Add sessions and cars routes to the API, implemented using the MongoDB datatabse

--- server.js
+++ server.js
@@ -1,20 +1,48 @@
 const express = require("express")
 const cors = require("cors")
 const port = process.env.PORT || 3000
-const MongoClient = require('mongodb').MongoClient;
+const mongo = require("mongodb")
 const uri = "mongodb+srv://matthew:Yq3gdiOOCgVRY411@smr-dashboard.wbqeq.mongodb.net/smr-dashboard?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new mongo.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express()
 app.use(cors())
 
 app.get("/sessions", (req, res) => {
 	client.connect(err => {
 		const collection = client.db("dashboard").collection("sessions")
-		collection.find({name: "VEC 2020 Race 4"}, (err, cursor) => {
-			cursor.toArray((err, cars) => {
-				res.send(cars)
-				client.close()
+		collection.find({}, (err, cursor) => {
+			cursor.toArray((err, sessions) => {
+				res.send(sessions)
 			})
+		})
+	})
+})
+
+app.get("/sessions/:sessionId", (req, res) => {
+	client.connect(err => {
+		const collection = client.db("dashboard").collection("sessions")
+		collection.findOne({"_id": mongo.ObjectID(req.params.sessionId)}, (err, session) => {
+			res.send(session)
+		})
+	})
+})
+
+app.get("/sessions/:sessionId/cars", (req, res) => {
+	client.connect(err => {
+		const collection = client.db("dashboard").collection("sessions")
+		collection.findOne({"_id": mongo.ObjectID(req.params.sessionId)}, (err, session) => {
+			res.send(session.cars)
+		})
+	})
+})
+
+app.get("/sessions/:sessionId/cars/:carNumber", (req, res) => {
+	client.connect(err => {
+		const collection = client.db("dashboard").collection("sessions")
+		collection.findOne({"_id": mongo.ObjectID(req.params.sessionId)}, (err, session) => {
+			res.send(session.cars.filter((car) => {
+				return car.number == req.params.carNumber
+			}))
 		})
 	})
 })
